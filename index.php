53d2f1cdfaf1f5e9479243af4f01fccf73d943f4
PRD-SG <mouth_hd@hotmail.co.th>
2022-03-20 15:10:49 +0700

    edit

--- index.php
+++ index.php
@@ -62,11 +62,9 @@
 
     <div style="margin-top:100px"></div>
     <footer class="bg-light text-center text-lg-start fixed-bottom">
-        <!-- Copyright -->
         <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
             © 2020 Copyright:
         </div>
-        <!-- Copyright -->
     </footer>
 </body>
 
